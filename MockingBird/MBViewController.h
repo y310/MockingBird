d2c31a0b7b3901b587b7252682c2d70878624bf8
Yusuke Mito <y310.1984@gmail.com>
2014-01-25 15:41:49 +0900

    Initial Commit

---
+++ MockingBird/MBViewController.h
@@ -0,0 +1,13 @@
+//
+//  MBViewController.h
+//  MockingBird
+//
+//  Created by mito on 2014/01/25.
+//  Copyright (c) 2014年 mito. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface MBViewController : UIViewController
+
+@end
---
+++ MockingBird/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  MockingBird
+//
+//  Created by mito on 2014/01/25.
+//  Copyright (c) 2014年 mito. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "MBAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([MBAppDelegate class]));
+    }
+}
